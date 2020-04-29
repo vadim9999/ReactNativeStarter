656b74eb761a8d0b46bfb7412f1539fe9c0b21d2
Vadim <vadim.putrov@gmail.com>
2020-04-29 19:17:17 +0300

    add dot env

--- app/store/sagas/spx-capsules-saga.ts
+++ app/store/sagas/spx-capsules-saga.ts
@@ -1,17 +1,35 @@
-import { put, all, takeEvery } from 'redux-saga/effects';
+import { put, all, takeEvery, call } from 'redux-saga/effects';
+import { REACT_APP_BACKEND_SPACEX_CAPSULES } from 'react-native-dotenv';
 
 import { getCapsulesSuccess } from '../actions';
 import { capsulesData } from '../api/spx-capsules-api';
 import { GET_CAPSULES } from '../constants';
+import { Capsule } from '../../models/spx-capsules-api-types';
+
+const getCapsules = async (): Promise<Capsule[]> => {
+  try {
+    const response: Response = await fetch(
+      `${REACT_APP_BACKEND_SPACEX_CAPSULES}`
+    );
+    const json: Capsule[] = await response.json();
+    return json;
+  } catch (error) {
+    console.log('error', error);
+  }
+
+  return [];
+};
 
 function* getCapsulesSaga() {
-  yield console.log("I am here");
-  
+  yield console.log('I am here');
+
   try {
-    const data = yield [...capsulesData];
-    yield put(getCapsulesSuccess(data));
+    // const data = yield [...capsulesData];
+    const capsules: Capsule[] = yield call(getCapsules);
+
+    yield put(getCapsulesSuccess(capsules));
   } catch (e) {
-    console.log('error');
+    console.log('error', e);
   }
 }
 
